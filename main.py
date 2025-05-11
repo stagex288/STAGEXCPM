0b8f386279e0da2e87279b99374b7641727f436c
stagex288 <stagexvendas01@gmail.com>
2025-05-11 19:47:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from noelcpm import CPMnoelcpm
+from stagexcpm import CPMnoelcpm
 
 __CHANNEL_USERNAME__ = "@noel_vendas"
 __GROUP_USERNAME__   = "11978458163"
